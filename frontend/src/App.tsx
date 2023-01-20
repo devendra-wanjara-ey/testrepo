7591b478065c906803c330703ec87c03cfcd7a50
devendra-wanjara-ey <devendra.wanjara@ey.com>
2023-01-20 15:26:06 -0500

    Squashed commit of the following:
    commit 64a5317ca5637ec6d85a68d09d350f6669671860
    Author: devendra-wanjara <devenwan.yahoo.com>
    Date:   Fri Jan 20 19:42:11 2023 +0000
    
        adding changes for Jan 20
    
    commit b25c87a1fb3467f11d5597159d150a1e812b81a4
    Merge: 5cd51c5 2371e43
    Author: devendra-wanjara-ey <98172246+devendra-wanjara-ey@users.noreply.github.com>
    Date:   Thu Jan 19 13:46:13 2023 -0500
    
        Merge pull request #1 from devendra-wanjara-ey/origin/minter-frontend
    
        Adding Mint changes
    
    commit 2371e43c932d6fccea8e28031d32374b52ee2aef
    Merge: 8b52f75 5cd51c5
    Author: devendra-wanjara-ey <98172246+devendra-wanjara-ey@users.noreply.github.com>
    Date:   Thu Jan 19 13:44:57 2023 -0500
    
        Merge branch 'main' into origin/minter-frontend
    
    commit 5cd51c56accc3393154de3d88b20c57e4f5b67b7
    Author: devendra-wanjara <devenwan.yahoo.com>
    Date:   Thu Jan 19 18:21:59 2023 +0000
    
        Truffle config changes adding 30 test accounts
    
    commit fe5242ee2bdfbaea56d0fb813b512f9dac203cbe
    Author: devendra-wanjara <devenwan.yahoo.com>
    Date:   Thu Jan 19 18:21:54 2023 +0000
    
        Updates ABI, added Ganache with 30 Address,
    
    commit 8b52f75ea3254e4094662b9e986cdced3bea02de
    Author: devendra-wanjara <devenwan.yahoo.com>
    Date:   Thu Jan 19 16:03:23 2023 +0000
    
        Adding Mint changes
    
    commit e44af136727daddc768906925c057055aa089500
    Author: devendra-wanjara <devenwan.yahoo.com>
    Date:   Thu Jan 19 15:54:47 2023 +0000
    
        Added new mint function with only one mint per address
    
    commit ef750379e029e68ba24b66ba701d662f7a18dc5c
    Author: devendra-wanjara-ey <98172246+devendra-wanjara-ey@users.noreply.github.com>
    Date:   Wed Jan 18 22:56:28 2023 -0500
    
        Update Team2NFT_3.json
    
    commit 447e1c548ae1ef537ed5fa6ec7775ce722a3fca6
    Author: devendra-wanjara-ey <98172246+devendra-wanjara-ey@users.noreply.github.com>
    Date:   Wed Jan 18 22:55:52 2023 -0500
    
        Update Team2NFT_2.json
    
    commit e09fda9cf275a8121498bd7151ee5e5d9a913bd8
    Author: devendra-wanjara-ey <98172246+devendra-wanjara-ey@users.noreply.github.com>
    Date:   Wed Jan 18 22:55:25 2023 -0500
    
        Update Team2NFT_1.json
    
    commit ba6ec52201ea31874f2a7ade44b587fb1024034d
    Author: devendra-wanjara-ey <98172246+devendra-wanjara-ey@users.noreply.github.com>
    Date:   Wed Jan 18 22:54:28 2023 -0500
    
        Update Team2NFT_0.json
    
    commit e9eb221f0dcb7164c5eeab66a9d1a3dde2c0436b
    Author: devendra-wanjara-ey <98172246+devendra-wanjara-ey@users.noreply.github.com>
    Date:   Wed Jan 18 22:54:11 2023 -0500
    
        Update Team2NFT_0.json

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,100 +1,39 @@
-import React, { useEffect, useState } from 'react';
-import Web3 from 'web3';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import { Team2NFTAbi } from './Team2NFTAbi';
+import Minter from './Minter'
+import App1 from './App1'
+import Tabs from './components/Tabs';
+declare module 'react' {
 
-interface Team2NFT {
-  tokenId: string
-  image: string,
-  traits: Record<string, number>
+  interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
+
+    // extends React's HTMLAttributes
+
+    label?: string;
+
+  }
+
 }
 
 function App() {
-  const [address, setAddress] = useState('');
-  const [ntfs, setNfts] = useState<Team2NFT[]>([]);
-  const [loading, setLoading] = useState(true);
-  const isValid = Web3.utils.isAddress(address);
-
-  useEffect(() => {
-    if (isValid) {
-      const fetchData = async () => {
-
-        //const web3 = new Web3("https://rinkeby.infura.io/v3/d4ed4c25a40645bd95f4d33bc7cd0925");
-        const web3 = new Web3("http://127.0.0.1:9545/");
-	console.log(Team2NFTAbi);
-        const contract = new web3.eth.Contract(Team2NFTAbi as any, "0xBF8Be22E3463EA4563F8CF1179e0Ceb12E4Aa658");
-
-        const balance = await contract.methods.balanceOf(address).call();
-	console.log(balance);
-
-        const newNfts: Team2NFT[] = [];
-        for (let ix = 0; ix < balance; ix++) {
-          const tokenId = await contract.methods.tokenOfOwnerByIndex(address, ix).call();
-          const tokenURI = await contract.methods.tokenURI(tokenId).call();
-          const metadataResponse = await fetch(tokenURI);
-          const metadata = await metadataResponse.json();
-          console.log(metadata);
-
-          const traits: Record<string, number> = {};
-
-          for (const attribute of metadata.attributes) {
-            traits[attribute.trait_type] = attribute.value;
-          }
-
-          newNfts.push({
-            tokenId,
-            image: metadata.image,
-            traits,
-          })
-        }
-
-
-        setNfts(newNfts)
-        setLoading(false);
-      }
-      fetchData();
-    }
-  }, [address, isValid])
-
-
   return (
-    <div className="container">
-      <h1 className='mt-3'>Our cool Web Team2 NFT viewer</h1>
-      <label>Address of the user ... that has alllll the NFTs</label>
-      <input type="text"
-        className={`form-control ${isValid ? '' : 'is-invalid'}`}
-        value={address}
-        onChange={e => setAddress(e.target.value)}
-      />
+    <div>
+      <h1>NFT Team 2</h1>
+      <br></br>
+      <Tabs>
 
-      {isValid && <div className='mt-5'>
-        <h3>These are all the cool NFTs</h3>
-
-        {loading
-          ? <p className='text-muted'>Loading ...</p>
-          : <div>
-            <p className='text-muted'>The address <span className='text-primary'>{address}</span> has <span className='text-primary'>{ntfs.length}</span> NFTs</p>
-            <div className='row'>
-              {ntfs.map(nft => <div className='col-4 border'>
-                <h5>#{nft.tokenId}</h5>
-                <img src={nft.image} alt='cleaver' className='w-100' />
-
-                <div className='row'>
-                  <div className='col'>
-                    H: {nft.traits.Hardworking}
-                  </div>
-                  <div className='col'>
-                    E: {nft.traits.Efficient}
-                  </div>
-                </div>
-              </div>)}
-            </div>
-          </div>}
-      </div>}
-
+      <div label="Main">
+           Please add Search 
+           <App1/>
+        </div>
+        <div label="Search NFT">
+           Please add Search 
+        </div>
+        <div label="Mint NFT">
+          <Minter/>
+        </div>
+        
+      </Tabs>
     </div>
   );
 }
-
 export default App;
--- frontend/src/Minter.tsx
+++ frontend/src/Minter.tsx
@@ -2,12 +2,15 @@
 import Web3 from "web3";
 import { Contract } from "web3-eth-contract";
 import 'bootstrap/dist/css/bootstrap.css';
+import { Team2NFTAbi } from './Team2NFTAbi';
+import { Team2NFTV1Abi } from './Team2NFTV1Abi';
 import { useState } from "react";
+import { Card } from "react-bootstrap";
 declare var window: any;
 
 var contract: Contract | null = null;
 var account: string | null  = null;
-const SMART_CONTRACT = "0x4080867b3941dC20977828025326B6364F2Be70B";
+const SMART_CONTRACT = "0xf73427FA6e4270e60F78531eA21dB9019c039E41";
 
 interface Team2NFT {
   tokenId: string
@@ -23,7 +26,6 @@ function Minter() {
   const [primarySkill, setPrimarySkill] = useState('');
   const [secondarySkill, setSecondarySkill] = useState('');
   const [empId, setEmpId] = useState('');
-  const [location, setLocation] = useState('');
   const [ntfs, setNfts] = useState<Team2NFT[]>([]);
 
   const handleContractChange = async () => {
@@ -42,515 +44,16 @@ function Minter() {
   };
 
   const mint = async() => {
-  
-        const web3 = new Web3(window.ethereum);
-	      contract = new web3.eth.Contract([
-          {
-            "inputs": [],
-            "stateMutability": "nonpayable",
-            "type": "constructor"
-          },
-          {
-            "anonymous": false,
-            "inputs": [
-              {
-                "indexed": true,
-                "internalType": "address",
-                "name": "owner",
-                "type": "address"
-              },
-              {
-                "indexed": true,
-                "internalType": "address",
-                "name": "approved",
-                "type": "address"
-              },
-              {
-                "indexed": true,
-                "internalType": "uint256",
-                "name": "tokenId",
-                "type": "uint256"
-              }
-            ],
-            "name": "Approval",
-            "type": "event"
-          },
-          {
-            "anonymous": false,
-            "inputs": [
-              {
-                "indexed": true,
-                "internalType": "address",
-                "name": "owner",
-                "type": "address"
-              },
-              {
-                "indexed": true,
-                "internalType": "address",
-                "name": "operator",
-                "type": "address"
-              },
-              {
-                "indexed": false,
-                "internalType": "bool",
-                "name": "approved",
-                "type": "bool"
-              }
-            ],
-            "name": "ApprovalForAll",
-            "type": "event"
-          },
-          {
-            "anonymous": false,
-            "inputs": [
-              {
-                "indexed": true,
-                "internalType": "address",
-                "name": "previousOwner",
-                "type": "address"
-              },
-              {
-                "indexed": true,
-                "internalType": "address",
-                "name": "newOwner",
-                "type": "address"
-              }
-            ],
-            "name": "OwnershipTransferred",
-            "type": "event"
-          },
-          {
-            "anonymous": false,
-            "inputs": [
-              {
-                "indexed": true,
-                "internalType": "address",
-                "name": "from",
-                "type": "address"
-              },
-              {
-                "indexed": true,
-                "internalType": "address",
-                "name": "to",
-                "type": "address"
-              },
-              {
-                "indexed": true,
-                "internalType": "uint256",
-                "name": "tokenId",
-                "type": "uint256"
-              }
-            ],
-            "name": "Transfer",
-            "type": "event"
-          },
-          {
-            "inputs": [
-              {
-                "internalType": "address",
-                "name": "to",
-                "type": "address"
-              },
-              {
-                "internalType": "uint256",
-                "name": "tokenId",
-                "type": "uint256"
-              }
-            ],
-            "name": "approve",
-            "outputs": [],
-            "stateMutability": "nonpayable",
-            "type": "function"
-          },
-          {
-            "inputs": [
-              {
-                "internalType": "address",
-                "name": "owner",
-                "type": "address"
-              }
-            ],
-            "name": "balanceOf",
-            "outputs": [
-              {
-                "internalType": "uint256",
-                "name": "",
-                "type": "uint256"
-              }
-            ],
-            "stateMutability": "view",
-            "type": "function"
-          },
-          {
-            "inputs": [
-              {
-                "internalType": "uint256",
-                "name": "tokenId",
-                "type": "uint256"
-              }
-            ],
-            "name": "getApproved",
-            "outputs": [
-              {
-                "internalType": "address",
-                "name": "",
-                "type": "address"
-              }
-            ],
-            "stateMutability": "view",
-            "type": "function"
-          },
-          {
-            "inputs": [
-              {
-                "internalType": "address",
-                "name": "owner",
-                "type": "address"
-              },
-              {
-                "internalType": "address",
-                "name": "operator",
-                "type": "address"
-              }
-            ],
-            "name": "isApprovedForAll",
-            "outputs": [
-              {
-                "internalType": "bool",
-                "name": "",
-                "type": "bool"
-              }
-            ],
-            "stateMutability": "view",
-            "type": "function"
-          },
-          {
-            "inputs": [
-              {
-                "internalType": "address",
-                "name": "to",
-                "type": "address"
-              }
-            ],
-            "name": "mint",
-            "outputs": [],
-            "stateMutability": "nonpayable",
-            "type": "function"
-          },
-          {
-            "inputs": [],
-            "name": "mint1",
-            "outputs": [],
-            "stateMutability": "nonpayable",
-            "type": "function"
-          },
-          {
-            "inputs": [],
-            "name": "name",
-            "outputs": [
-              {
-                "internalType": "string",
-                "name": "",
-                "type": "string"
-              }
-            ],
-            "stateMutability": "view",
-            "type": "function"
-          },
-          {
-            "inputs": [],
-            "name": "owner",
-            "outputs": [
-              {
-                "internalType": "address",
-                "name": "",
-                "type": "address"
-              }
-            ],
-            "stateMutability": "view",
-            "type": "function"
-          },
-          {
-            "inputs": [
-              {
-                "internalType": "uint256",
-                "name": "tokenId",
-                "type": "uint256"
-              }
-            ],
-            "name": "ownerOf",
-            "outputs": [
-              {
-                "internalType": "address",
-                "name": "",
-                "type": "address"
-              }
-            ],
-            "stateMutability": "view",
-            "type": "function"
-          },
-          {
-            "inputs": [],
-            "name": "renounceOwnership",
-            "outputs": [],
-            "stateMutability": "nonpayable",
-            "type": "function"
-          },
-          {
-            "inputs": [
-              {
-                "internalType": "address",
-                "name": "to",
-                "type": "address"
-              },
-              {
-                "internalType": "string",
-                "name": "uri",
-                "type": "string"
-              }
-            ],
-            "name": "safeMint",
-            "outputs": [],
-            "stateMutability": "nonpayable",
-            "type": "function"
-          },
-          {
-            "inputs": [
-              {
-                "internalType": "address",
-                "name": "from",
-                "type": "address"
-              },
-              {
-                "internalType": "address",
-                "name": "to",
-                "type": "address"
-              },
-              {
-                "internalType": "uint256",
-                "name": "tokenId",
-                "type": "uint256"
-              }
-            ],
-            "name": "safeTransferFrom",
-            "outputs": [],
-            "stateMutability": "nonpayable",
-            "type": "function"
-          },
-          {
-            "inputs": [
-              {
-                "internalType": "address",
-                "name": "from",
-                "type": "address"
-              },
-              {
-                "internalType": "address",
-                "name": "to",
-                "type": "address"
-              },
-              {
-                "internalType": "uint256",
-                "name": "tokenId",
-                "type": "uint256"
-              },
-              {
-                "internalType": "bytes",
-                "name": "data",
-                "type": "bytes"
-              }
-            ],
-            "name": "safeTransferFrom",
-            "outputs": [],
-            "stateMutability": "nonpayable",
-            "type": "function"
-          },
-          {
-            "inputs": [
-              {
-                "internalType": "address",
-                "name": "operator",
-                "type": "address"
-              },
-              {
-                "internalType": "bool",
-                "name": "approved",
-                "type": "bool"
-              }
-            ],
-            "name": "setApprovalForAll",
-            "outputs": [],
-            "stateMutability": "nonpayable",
-            "type": "function"
-          },
-          {
-            "inputs": [
-              {
-                "internalType": "bytes4",
-                "name": "interfaceId",
-                "type": "bytes4"
-              }
-            ],
-            "name": "supportsInterface",
-            "outputs": [
-              {
-                "internalType": "bool",
-                "name": "",
-                "type": "bool"
-              }
-            ],
-            "stateMutability": "view",
-            "type": "function"
-          },
-          {
-            "inputs": [],
-            "name": "symbol",
-            "outputs": [
-              {
-                "internalType": "string",
-                "name": "",
-                "type": "string"
-              }
-            ],
-            "stateMutability": "view",
-            "type": "function"
-          },
-          {
-            "inputs": [
-              {
-                "internalType": "uint256",
-                "name": "index",
-                "type": "uint256"
-              }
-            ],
-            "name": "tokenByIndex",
-            "outputs": [
-              {
-                "internalType": "uint256",
-                "name": "",
-                "type": "uint256"
-              }
-            ],
-            "stateMutability": "view",
-            "type": "function"
-          },
-          {
-            "inputs": [
-              {
-                "internalType": "address",
-                "name": "owner",
-                "type": "address"
-              },
-              {
-                "internalType": "uint256",
-                "name": "index",
-                "type": "uint256"
-              }
-            ],
-            "name": "tokenOfOwnerByIndex",
-            "outputs": [
-              {
-                "internalType": "uint256",
-                "name": "",
-                "type": "uint256"
-              }
-            ],
-            "stateMutability": "view",
-            "type": "function"
-          },
-          {
-            "inputs": [
-              {
-                "internalType": "uint256",
-                "name": "tokenId",
-                "type": "uint256"
-              }
-            ],
-            "name": "tokenURI",
-            "outputs": [
-              {
-                "internalType": "string",
-                "name": "",
-                "type": "string"
-              }
-            ],
-            "stateMutability": "pure",
-            "type": "function"
-          },
-          {
-            "inputs": [],
-            "name": "totalSupply",
-            "outputs": [
-              {
-                "internalType": "uint256",
-                "name": "",
-                "type": "uint256"
-              }
-            ],
-            "stateMutability": "view",
-            "type": "function"
-          },
-          {
-            "inputs": [
-              {
-                "internalType": "address",
-                "name": "from",
-                "type": "address"
-              },
-              {
-                "internalType": "address",
-                "name": "to",
-                "type": "address"
-              },
-              {
-                "internalType": "uint256",
-                "name": "tokenId",
-                "type": "uint256"
-              }
-            ],
-            "name": "transferFrom",
-            "outputs": [],
-            "stateMutability": "nonpayable",
-            "type": "function"
-          },
-          {
-            "inputs": [
-              {
-                "internalType": "address",
-                "name": "newOwner",
-                "type": "address"
-              }
-            ],
-            "name": "transferOwnership",
-            "outputs": [],
-            "stateMutability": "nonpayable",
-            "type": "function"
-          }
-        ], SMART_CONTRACT);
-        console.log("add >"+address) 
-
-        const data = await contract.methods.mint(address).encodeABI();
-
-        console.log("data >>"+data);
-
-
-        const params = {
-            from: address,
-            to: SMART_CONTRACT,
-            data: data,
-            tokenURI : "{name:'TEST1'}"
-        };
+    try{
         
-        console.log("params >>"+params);
-
-        window.ethereum.request({
-            method: 'eth_sendTransaction',
-            params: [params]
-        }).then((res: string) => {
-            console.log("res >>"+res);
-        });
-
-      
+        const web3 = new Web3(window.ethereum);
+	      const contract = new web3.eth.Contract(Team2NFTV1Abi as any, SMART_CONTRACT);
+        console.log("add >"+address) 
+        contract.methods.mint(account).send({ from: account})
+        
+    }catch(ex){
+      console.log(ex)
+    }
   }
 
 
@@ -589,26 +92,19 @@ function Minter() {
           &nbsp;<input
           type="text"
           placeholder="XXXXX" value={empId}
-          onChange={(e) => setEmpId(e.target.value)}
+          onChange={(e) => setName(e.target.value)}
         />
         </h2>
         <h2>Primary Skills: 
             &nbsp;<input
           type="text" value ={primarySkill}
-          placeholder="e.g. UI/ Backend / Manager etc." onChange={(e) => setPrimarySkill(e.target.value)}
+          placeholder="e.g. UI/ Backend / Manager etc." onChange={(e) => setName(e.target.value)}
         />
         </h2>
         <h2>Secondary Skills: 
             &nbsp;<input
           type="text" value ={secondarySkill}
-          placeholder="e.g. UI/ Backend / Manager etc." onChange={(e) => setSecondarySkill(e.target.value)}
-        />
-        </h2>
-
-        <h2>Location: 
-            &nbsp;<input
-          type="text" value ={location}
-          placeholder="Texas" onChange={(e) => setLocation(e.target.value)}
+          placeholder="e.g. UI/ Backend / Manager etc." onChange={(e) => setName(e.target.value)}
         />
         </h2>
   
